dfda7a60c0f528cf7191719b1e6264922aec149b
Olivier Maréchal <marechalolivier84@gmail.com>
2023-05-01 18:01:21 +0200

    first navbar

--- src/layout/header/Header.jsx
+++ src/layout/header/Header.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import logo from "../img/logo.png"
+import logoCompte from "../img/logo_compte.png"
 import { Link } from "react-router-dom";
 import style from "../header/style.scss"
 
@@ -7,41 +9,17 @@ const Header =() => {
   return (
     <>
       <header>
-        <h1>Hello</h1>
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-          <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="#">Home</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Dropdown
-                  </a>
-                  <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="#">Action</a></li>
-                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                    <li><hr class="dropdown-divider"/></li>
-                    <li><a class="dropdown-item" href="#">Something else here</a></li>
-                  </ul>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link disabled">Disabled</a>
-                </li>
-              </ul>
-              <form class="d-flex" role="search">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                <button class="btn btn-outline-success" type="submit">Search</button>
-              </form>
-            </div>
+        <nav>
+          <div className="logo">
+            <img src={logo} alt="Logo de l'entreprise"/>
+          </div>
+          <div class="search">
+            <input type="text" placeholder="Recherche"/>
+            <button type="submit">Rechercher</button>
+          </div>
+          <div className="account">
+            <img src={logoCompte}  alt="logo compte"/>
+            <a href="#">Mon compte</a>
           </div>
         </nav>
       </header>
