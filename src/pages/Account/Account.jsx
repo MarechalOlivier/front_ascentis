97ed4ab0edb2e2aa445fa613391118ee2a1cbed0
Olivier Maréchal <marechalolivier84@gmail.com>
2023-05-12 00:17:53 +0200

    My account ok

--- src/pages/Account/Account.jsx
+++ src/pages/Account/Account.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import Header from "../../layout/header/Header";
 import Footer from "../../layout/footer/Footer";
+import "./style_account.scss";
 
 const Account =() => {
 
@@ -22,7 +23,48 @@ const Account =() => {
     return(
         <>
             <Header />
-            <h1>Mon compte</h1>
+            <main>
+                <section id="account">
+                    <h1>Mon compte</h1>
+                    
+                    <div class="content-account">
+
+                        <div class="ticket">
+                            <div class="line-ticket">
+                                <h3>Statut réparation</h3>
+                                <p>Vérifier l'état de réparation de vos produits</p>
+                            </div>
+                            <div class="line-ticket">
+                                <h3>Créer un ticket</h3>
+                                <p>Ouvrir un ticket de support pour signaler un problème</p>
+                                <button class="btn btn-primary"><Link to="/admin/create_ticket">Créez un ticket</Link></button>
+                            </div>
+                        </div>
+                        <div class="infos">
+                            <div class="line">
+                                <h3>Mes informations personnelles</h3>
+                                <p>Gérer vos informations personnelles</p>
+                                <button class="btn btn-primary">Modifier</button>
+                            </div>
+                            <div class="line">
+                                <h3>Mes adresses de livraison</h3>
+                                <p>Gérer vos adresses de livraison pour vos commandes</p>
+                                <button class="btn btn-primary">Modifier</button>
+                            </div>
+                            <div class="line">
+                                <h3>Mes commandes et factures</h3>
+                                <p>Consulter vos commandes et télécharger vos factures</p>
+                                <button class="btn btn-primary">Consulter</button>
+                            </div>
+                            <div class="line">
+                                <h3>Préférences</h3>
+                                <p>Gérer vos préférences de compte</p>
+                                <button class="btn btn-primary">Modifier</button>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </main>
             <Footer />
         </>
     )
